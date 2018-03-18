520363f282bb015ce0c72a5668ba9dd4e91ba3a7
ItakuraKoji <kouzi85@gmail.com>
2018-03-18 20:48:54 +0900

    ファイル上げ
    Unityの作業ファイル全部上げー

---
+++ Assets/PlayerFoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFoot : MonoBehaviour {
+    public bool stayGround;
+
+	// Use this for initialization
+	void Start () {
+        stayGround = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Ground")
+        {
+            stayGround = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        stayGround = false;
+    }
+}

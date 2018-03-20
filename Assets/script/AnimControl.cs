26501bbe5628174c8b02383b924618a175afa455
tonarinohito <rsp44316@gmail.com>
2018-03-20 22:30:24 +0900

    ユニティちゃん

---
+++ Assets/script/AnimControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour {
+
+    private Animator animator;
+    public GameObject obj;
+    private PlayerControler player;
+    float y;
+    // Use this for initialization
+        void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = obj.GetComponent<PlayerControler>();
+        y = 90;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        if (Input.GetButtonDown("Fire1") && player.isJumping)
+        {
+            animator.SetBool("isjump", true);
+        }
+      else if(!player.isJumping)
+        {
+            animator.SetBool("isjump", false);
+        }
+      if(Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0)
+        {
+            animator.SetBool("isrun", true);
+        }
+      else
+        {
+            animator.SetBool("isrun", false);
+        }
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            if(y == -90)
+            {
+                transform.Rotate(0, 180, 0);
+                y = 90;
+            }
+        }
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            if(y == 90)
+            {
+                transform.Rotate(0, -180, 0);
+                y = -90;
+            }
+           
+        }
+    }
+}

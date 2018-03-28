57cedbcd8041a6e28e58cc70545015fe2ffd28b8
tonarinohito <rsp44316@gmail.com>
2018-03-28 23:23:30 +0900

    Revert "Revert "Goal追加""
    This reverts commit 73df937b6ef10723f8a693aa3d6a7ff1740ac867.

---
+++ Assets/script/Goal/Goal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Goal : MonoBehaviour {
+    public GameObject goal;
+    public GUIStyle goalText;
+    public bool isGoal;
+	// Use this for initialization
+	void Start () {
+        goalText = new GUIStyle();
+        goalText.fontSize = 100;
+        isGoal = false;
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isGoal = true;
+            Debug.Log("Goal!!!!!!");
+        }
+    }
+    private void OnGUI()
+    {
+        if (isGoal)
+        {
+            
+            goalText.normal.textColor = new Color(255f, 0, 255f);
+            GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "続きは製品版で", goalText);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}

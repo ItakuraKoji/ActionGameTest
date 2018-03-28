f67f36c940ddc7ae09b9ddb1e6dd33c502e78102
ItakuraKoji <kouzi85@gmail.com>
2018-03-28 16:15:07 +0900

    Revert "衝突回避"
    This reverts commit ba113d5c84b7822267dd500fbd4b06361fa76d3b.

---
+++ Assets/script/Player/PlayerControler.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+    const int MAXSKILLNUM = 4;
+    CharacterController controler;
+
+
+    float speed;
+    float glavity;
+    float JumpPower;
+    float vertVelosity;
+    float minVertVelosity;
+    public bool isJumping;
+
+    public AudioClip jumpclip;
+    private AudioSource jumpSe;
+    public PlayerFoot foot;
+    public GameObject skillObj;
+    private Skill skill;// = new Skill();
+    SkillType[] id = new SkillType[MAXSKILLNUM];
+
+    float direction = 90f;
+    float summonX = 3.6f;
+    // Use this for initialization
+    void Start () {
+        this.controler = this.gameObject.GetComponent<CharacterController>();
+        this.speed = 0.8f;
+        this.glavity = 0.05f;
+        this.JumpPower = 1.0f;
+        this.vertVelosity = 0.0f;
+        this.minVertVelosity = -1.0f;
+        this.jumpSe = gameObject.AddComponent<AudioSource>();
+        this.jumpSe.clip = this.jumpclip;
+        this.isJumping = false;
+        for(int i = 0; i < MAXSKILLNUM; ++i)
+        {
+            this.id[i] = SkillType.NONE;
+        }
+        //仮処理。撮影したらこんな感じになってその動作が使えるようになる
+        this.id[1] = SkillType.HIGH_JUMP;
+        this.id[2] = SkillType.PUNCH;
+        this.id[3] = SkillType.SLASH;
+
+        //追加
+        skill = skillObj.GetComponent<Skill>();
+    }
+	void SkillActivate()
+    {
+        for (int i = 0; i < MAXSKILLNUM; ++i)
+        {
+            switch (this.id[i])
+            {
+                case SkillType.NONE:
+                    break;
+                case SkillType.HIGH_JUMP:
+                    skill.HighJump(ref JumpPower,this.isJumping);
+               
+                    break;
+                case SkillType.PUNCH:
+                    break;
+                case SkillType.SLASH:
+                    Vector3 pos = new Vector3(
+                        this.transform.position.x + summonX,
+                        this.transform.position.y,
+                        this.transform.position.z);
+                    skill.Slash(pos,new Vector3(0,direction,0));
+                    break;
+            }
+        }
+
+    }
+
+    
+    // Update is called once per frame
+    void Update () {
+        Vector3 vector = new Vector3(0.0f, 0.0f, 0.0f);
+
+        float axis = Input.GetAxis("Horizontal");
+        vector += new Vector3(axis * speed, 0.0f, 0.0f);
+
+       
+       if(axis < 0) { direction = -90;  summonX = -4.6f; }
+       if(axis > 0) { direction = 90;  summonX = 4.6f; }
+
+        SkillActivate();
+        if (Input.GetButtonDown("Fire1") && this.foot.stayGround)
+        {
+
+            jumpSe.Play();
+            this.isJumping = true;
+            this.vertVelosity = this.JumpPower;
+        }
+
+        if (this.foot.stayGround && this.vertVelosity < -0.1f)
+        {
+            this.vertVelosity = -0.1f;
+            this.isJumping = false;
+        }
+        vector += new Vector3(0.0f, this.vertVelosity, 0.0f);
+        this.vertVelosity -= this.glavity;
+        if(this.vertVelosity < this.minVertVelosity)
+        {
+            this.vertVelosity = this.minVertVelosity;
+        }
+
+        this.controler.Move(vector);
+    }
+}
---
+++ Assets/script/cameraGun.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraGun : MonoBehaviour
+{
+
+    public Jump jumpObj;
+    public SkeletonAnim sword;
+    public GameObject gun;
+    public GameObject player;
+
+    public GameObject attackObj;
+    private CreateAnim createObj;
+    
+    //
+    bool isTrigger;
+    bool isjumpHit;
+    bool isSwordHit;
+    float speed;
+    int dir;
+    int time;
+    public int type;
+    //
+
+    const int MAXTIME = 60;
+    // Use this for initialization
+    void Start()
+    {
+        if(type == 1)
+        {
+            gun.GetComponent<MeshRenderer>().enabled = false;
+        }
+        
+        isTrigger = false;
+        isjumpHit = false;
+        isSwordHit = false;
+        dir = 1;
+        speed = 1f;
+        time = MAXTIME;
+        createObj = attackObj.GetComponent<CreateAnim>();
+       
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Skeleton" ||
+             other.gameObject.tag == "Goblin")
+        {
+            gun.GetComponent<Renderer>().material.color = new Color(255, 255, 0);
+          
+        }
+         //カメラのシャッターを押す
+        if (other.gameObject.tag == "Goblin")
+        {
+
+            isjumpHit = true;
+            if (jumpObj.jumpFlag)
+            {
+                gun.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+              
+                //使用回数を+5する
+                if (Input.GetButtonDown("Fire3"))
+                {
+                    Skill.quantity[(int)SkillType.HIGH_JUMP] += 5;
+                    Debug.Log("ジャンプ+5");
+                }
+            }
+        }
+        if (other.gameObject.tag == "Skeleton")
+        {
+            isSwordHit = true;
+            if (sword.animState == SkeletonAnim.AnimState.Attack)
+            {
+           
+                gun.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+                if (Input.GetButtonDown("Fire3"))
+                {
+                    Skill.quantity[(int)SkillType.SLASH] += 5;
+                    Debug.Log("スラッシュ+5");
+                }
+            }
+        }
+    }
+
+   void FixedUpdate()
+    {
+        //このフレームにおける初期値を設定
+        //そのあとに判定があったら値を上書きしている
+        isSwordHit = false;
+        isjumpHit = false;
+        gun.GetComponent<Renderer>().material.color = new Color(255, 255, 255);
+    }
+
+    //プレイヤーの動きに連動してるので、プレイヤーが動いた後に更新を行うのが好ましい
+    void LateUpdate()
+    {
+        Vector3 pos = player.transform.position;    //プレイヤーの位置を取得
+        if (type == 0)
+        {
+            //カメラ向き
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                gun.transform.position = new Vector3(pos.x + 3.6f, pos.y, pos.z);
+
+            }
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                gun.transform.position = new Vector3(pos.x - 3.6f, pos.y, pos.z);
+
+            }
+        }
+
+        if (type == 1)
+        {
+            //カメラ向き
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                dir = 1;
+            }
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                dir = -1;
+            }
+            if (time >= MAXTIME)
+            {
+                gun.transform.position = new Vector3(pos.x, pos.y, pos.z);
+            }
+            if (Input.GetButtonDown("Fire3"))
+            {
+                isTrigger = true;
+
+            }
+            if (isTrigger)
+            {
+                if (isjumpHit)
+                {
+                    gun.transform.position = jumpObj.transform.position;
+                }
+                if (isSwordHit)
+                {
+                    gun.transform.position = sword.transform.position;
+                }
+                if (!isSwordHit && !isjumpHit)
+                {
+                    gun.transform.position += new Vector3(speed * dir, 0, 0);
+                }
+                gun.GetComponent<MeshRenderer>().enabled = true;
+                --time;
+                if (time <= 0)
+                {
+                    gun.GetComponent<MeshRenderer>().enabled = false;
+                    isTrigger = false;
+                    time = MAXTIME;
+                }
+            }
+        }
+
+    }
+
+}
